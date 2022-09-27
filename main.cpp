a71e8cd091c566b38e2c62b2a5900b2ae8d08a3a
Kateryna <ketrin01092004@gmail.com>
2022-09-27 20:06:11 +0300

    third task

--- main.cpp
+++ main.cpp
@@ -16,14 +16,20 @@ int main() {
     int min = INFINITY;
     srand(int(time(nullptr))%60);
     int len = userInput();
-    for (int i = 0; i < len; ++i) {
+    int s = 0;
+    for (int i = 0; i < len; i++) {
         int n = rand();
+
         if (n <= min)
             min = n;
         else if (n>=max)
             max = n;
+
+        if (n%2==0)
+            s += n;
+
         cout << n <<endl;
     }
-    cout<<"min: "<<min<<endl<<"max: "<<max;
+    cout<<"min: "<<min<<endl<<"max: "<<max<<endl<<"sum of even numbers: "<<s;
     return 0;
 }
